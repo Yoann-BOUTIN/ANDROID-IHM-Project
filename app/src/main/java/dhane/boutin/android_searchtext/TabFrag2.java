0dba5bcc29a6fbf94e9716ddef2553c1cd7f2b2c
Yoann BOUTIN <boutinyoann@gmail.com>
2015-10-15 10:52:57 +0200

    add 3 tabs for different scroll test

---
+++ app/src/main/java/dhane/boutin/android_searchtext/TabFrag2.java
@@ -0,0 +1,18 @@
+package dhane.boutin.android_searchtext;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bouti on 15/10/2015.
+ */
+public class TabFrag2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_frag_2, container, false);
+    }
+}
