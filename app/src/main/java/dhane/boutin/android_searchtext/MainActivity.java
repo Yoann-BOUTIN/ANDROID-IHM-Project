0258b69a8ab20833d871dab6de92fed35fc64086
Yoann BOUTIN <boutinyoann@gmail.com>
2015-10-15 11:58:15 +0200

    normal scroll tab1

--- app/src/main/java/dhane/boutin/android_searchtext/MainActivity.java
+++ app/src/main/java/dhane/boutin/android_searchtext/MainActivity.java
@@ -5,8 +5,11 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
